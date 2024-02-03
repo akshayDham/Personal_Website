a8b1494797841d19cbf66b3a739e3dd54c9121db
akshayDham <123764889+akshayDham@users.noreply.github.com>
2024-02-03 00:08:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// Enhanced JavaScript for responsive navbar
+document.addEventListener('DOMContentLoaded', function() {
+    // Add a toggle button for small screens
+    var navbar = document.querySelector('.navbar');
+    var toggleButton = document.createElement('a');
+    toggleButton.innerHTML = '☰';
+    toggleButton.style.display = 'none';
+    toggleButton.onclick = function() {
+        var links = navbar.querySelectorAll('a');
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].style.display === 'block') {
+                links[i].style.display = 'none';
+            } else {
+                links[i].style.display = 'block';
+            }
+        }
+    };
+// http://people.tamu.edu/~akshay123456789/
+
+//This is link to my landing page.
+
+ 
+
+ 
+
+//a link to your HTML ONLY landing page http://people.tamu.edu/~akshay123456789/index_html_only.html
+
+ 
+
+// https://github.com/tamu-edu-students/ImpCode.gitLinks to an external site.
+
+
+
+    navbar.insertBefore(toggleButton, navbar.firstChild);
+    window.onresize = function() {
+        if (window.innerWidth > 600) {
+            var links = navbar.querySelectorAll('a');
+            for (var i = 0; i < links.length; i++) {
+                links[i].style.display = 'block';
+            }
+            toggleButton.style.display = 'none';
+        } else {
+            toggleButton.style.display = 'block';
+        }
+    };
+    window.onresize(); // Call once to set initial state
+});
